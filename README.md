bcf19c27ea24d44faaf921aaa4294f02a005f9cf
Mararok <mararok@gmail.com>
2016-01-02 12:31:08 +0100

    Added build toolset
    Now EpicCore is Bukkit Plugin

---
+++ README.md
@@ -0,0 +1,1 @@
+EpicCore is a plugin for Spigot(Bukkit) and contains utility and features that other plugins make use of.
---
+++ src/com/mararok/epiccore/EpicCorePlugin.java
@@ -0,0 +1,11 @@
+/**
+ * EpicCore
+ * The MIT License
+ * Copyright (C) 2016 Mararok <mararok@gmail.com>
+ */
+package com.mararok.epiccore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EpicCorePlugin extends JavaPlugin {
+}
