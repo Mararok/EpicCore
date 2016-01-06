a320bfed516b5a3570a9ed5fc89e934420c6f844
Mararok <mararok@gmail.com>
2016-01-06 11:16:51 +0100

    Changed to generated queries

--- src/com/mararok/epiccore/database/DMQL.java
+++ src/com/mararok/epiccore/database/DMQL.java
@@ -5,31 +5,43 @@
  */
 package com.mararok.epiccore.database;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Helper for simple sql queries(insert,update,delete,select)
  */
 public class DMQL {
-  public CachedQuery insert;
-  public CachedQuery update;
-  public CachedQuery delete;
-  public CachedQuery select;
-
-  public DatabaseConnection connection;
+  private DatabaseConnection connection;
 
   public DMQL(DatabaseConnection connection) {
     this.connection = connection;
   }
 
-  public void initQueries() throws SQLException {
-    insert = connection.prepareCachedQuery("INSERT INTO ?(?) VALUES(?);");
-    insert.compile();
-    update = connection.prepareCachedQuery("UPDATE ? SET ? WHERE ?;");
-    update.compile();
-    delete = connection.prepareCachedQuery("DELETE ? WHERE ?;");
-    delete.compile();
-    select = connection.prepareCachedQuery("SELECT ? FROM ? WHERE ?;");
-    select.compile();
+  public int insert(String tableName, String columns, String values) throws SQLException {
+    Statement query = connection.query();
+    query.executeUpdate("INSERT " + tableName + "(" + columns + ") VALUES(" + values + ")");
+    ResultSet result = query.getGeneratedKeys();
+    return (result.next()) ? result.getInt(1) : 0;
   }
+
+  public ResultSet select(String columns, String from, String where) throws SQLException {
+    String sql = "SELECT " + columns + " FROM " + from + ((where != null) ? " WHERE " + where : "");
+    try {
+      return connection.query().executeQuery(sql);
+    } catch (SQLException e) {
+      throw new SQLException(sql);
+    }
+
+  }
+
+  public int update(String tableName, String set, String where) throws SQLException {
+    return connection.exec("UPDATE " + tableName + " SET " + set + " WHERE " + where);
+  }
+
+  public int delete(String from, String where) throws SQLException {
+    return connection.exec("DELETE " + from + " WHERE " + where);
+  }
+
 }
