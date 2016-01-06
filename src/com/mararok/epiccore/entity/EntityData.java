a59e8d0ea3f81503f992a65a006f8c696344409f
Mararok <mararok@gmail.com>
2016-01-06 11:21:37 +0100

    Removed method toList

--- src/com/mararok/epiccore/entity/EntityData.java
+++ src/com/mararok/epiccore/entity/EntityData.java
@@ -5,17 +5,7 @@
  */
 package com.mararok.epiccore.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class EntityData {
   public int id;
 
-  public List<String> toStringList() {
-    List<String> list = new ArrayList<String>();
-    if (id != 0) {
-      list.add(Integer.toString(id));
-    }
-    return list;
-  }
 }
